e94d7d4de21d3dff410a1c6f6ce5203c37cbfbbd
Nikita Zabelin <info@sim-roulette.com>
2020-11-06 16:44:57 +0300

    fix

--- cards.php
+++ cards.php
@@ -1,6 +1,5 @@
 <?
 // ===================================================================
-// Sim Roulette -> Settings
 // License: GPL v3 (http://www.gnu.org/licenses/gpl.html)
 // Copyright (c) 2016-2020 Xzero Systems, http://sim-roulette.com
 // Author: Nikita Zabelin
