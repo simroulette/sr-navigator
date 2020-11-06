05fd2645196e686e91ed3c90bd7b8d77aca4efd2
Nikita Zabelin <info@sim-roulette.com>
2020-11-06 16:42:39 +0300

    fix

--- actions.php
+++ actions.php
@@ -1,4 +1,11 @@
 <?
+// ===================================================================
+// Sim Roulette -> Settings
+// License: GPL v3 (http://www.gnu.org/licenses/gpl.html)
+// Copyright (c) 2016-2020 Xzero Systems, http://sim-roulette.com
+// Author: Nikita Zabelin
+// ===================================================================
+
 include("_func.php");
 $status=1;
 if ($_POST['delete']) // Deletes the selected actions | Удаление отмеченных задач
